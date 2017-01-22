47cd143363264810ae5f06e413f427388f0796bc
Stefan Kroneis <skroneis@yahoo.com>
2017-01-22 13:32:26 +0100

    :rabbit:

--- netatmoHelper.js
+++ netatmoHelper.js
@@ -20,53 +20,61 @@ var self = null;
 // =======================
 var WData = module.exports = {
     // temp: "",
-	// co2: "",
+    // co2: "",
     // getData: function() {
-	// 	console.log("getData...");
+    // 	console.log("getData...");
     //     WData.temp = "12,13";
-	// 	WData.co2 = "333";
+    // 	WData.co2 = "333";
     //  },
-	//  getMeasureTemp2 : function (callback){
-	// 	callback("1234");
-	//  },
-	 getMeasuresIn : function (callback) {
-		_options = optionsIndoor;		
-		api.getMeasure(_options, function (err, measure) {
-        // When the "error" event is emitted, this is called
-        //console.error('Netatmo threw an error: ' + error);
-        //console.log(measure[0].value);
-        var time = measure[0].beg_time;
-        var temp = measure[0].value[0][0];
-		var co2 = measure[0].value[0][1];
-		var humidity = measure[0].value[0][2];
-		var pressure = measure[0].value[0][3];
-		//console.log("_co2: %s", _co2);
-		// logger.info(measure[0].value);
-        // console.log("Temp: %s", temp);
-        callback(time, temp, co2, humidity, pressure); // this will "return" your value to the original caller
-		});
-	 },
-	 getMeasuresOut : function (callback) {
-		_options = optionsOutdoor;		
-		api.getMeasure(_options, function (err, measure) {
-        // When the "error" event is emitted, this is called
-        //console.error('Netatmo threw an error: ' + error);
-        // console.log(measure[0].value);
-        var time = measure[0].beg_time;
-        var temp = measure[0].value[0][0];
-		var humidity = measure[0].value[0][1];				
-        callback(time, temp, humidity); 
-		});
-	 },
-     init : function ()
-     {
+    //  getMeasureTemp2 : function (callback){
+    // 	callback("1234");
+    //  },
+    getMeasuresIn: function (callback) {
+        _options = optionsIndoor;
+        api.getMeasure(_options, function (err, measure) {
+            // When the "error" event is emitted, this is called
+            //console.error('Netatmo threw an error: ' + error);
+            //console.log(measure[0].value);
+            var time = measure[0].beg_time;
+            var temp = measure[0].value[0][0];
+            var co2 = measure[0].value[0][1];
+            var humidity = measure[0].value[0][2];
+            var pressure = measure[0].value[0][3];
+            //console.log("_co2: %s", _co2);
+            // logger.info(measure[0].value);
+            // console.log("Temp: %s", temp);
+            callback(time, temp, co2, humidity, pressure); // this will "return" your value to the original caller
+        });
+    },
+    getMeasuresOut: function (callback) {
+        _options = optionsOutdoor;
+        api.getMeasure(_options, function (err, measure) {
+            // When the "error" event is emitted, this is called
+            //console.error('Netatmo threw an error: ' + error);
+            // console.log(measure[0].value);
+            var time = measure[0].beg_time;
+            var temp = measure[0].value[0][0];
+            var humidity = measure[0].value[0][1];
+            callback(time, temp, humidity);
+        });
+    },
+    init: function () {
         self = this;
-        this.api = new netatmo(auth);
-        console.log ("init netatmo - OK");
-     }
-	 //,
+        //this.api = new netatmo(auth);
+        // var auth = api.authenticate(auth, function (err, infos) {
+        //     if (err)
+        //         console.log(err);
+        //     if (infos)
+        //         console.log(infos);
+        // })
+
+        // console.log (auth);
+        api.authenticate(auth);
+        console.log("init netatmo - OK");
+    }
+    //,
     // remove: function() {
-        // Counter.count += 10;
+    // Counter.count += 10;
     // }
 }
 
@@ -74,12 +82,12 @@ var WData = module.exports = {
 api.on("error", function (error) {
     // When the "error" event is emitted, this is called
     console.error('Netatmo threw an error: ' + error);
-	logger.error('Netatmo threw an error: ' + error);
+    logger.error('Netatmo threw an error: ' + error);
 });
 api.on("warning", function (error) {
     // When the "warning" event is emitted, this is called
     console.log('Netatmo threw a warning: ' + error);
-	logger.warn('Netatmo threw an warning: ' + error);
+    logger.warn('Netatmo threw an warning: ' + error);
 });
 
 var optionsIndoor = {
@@ -94,12 +102,12 @@ var optionsOutdoor = {
     scale: 'max',
     date_end: 'last',
     module_id: '02:00:00:00:f9:28',//'NAModule1',
-    type: ['Temperature','Humidity'],
+    type: ['Temperature', 'Humidity'],
 };
 
 
 // exports.getData = function() {
-    // return 'Hello!';
+// return 'Hello!';
 // }
 
 //re-init at midnight
